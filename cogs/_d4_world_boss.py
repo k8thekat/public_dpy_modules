70af0c466c6342eafc4e3e4091808f056c570cea
k8thekat <cadwalladerkatelynn@gmail.com>
2023-10-12 18:56:53 -0700

    Fixed some logic and fixed timing assumptions.

--- cogs/_d4_world_boss.py
+++ cogs/_d4_world_boss.py
@@ -1,21 +1,20 @@
-
-import pytz
+from datetime import datetime, timedelta, tzinfo
 from pathlib import Path
-from datetime import datetime, timedelta
 import json
 from typing_extensions import Union, Any, Self
 import copy
+import pytz
 
 
 class WorldBoss():
-    def __init__(self, boss_index: int = 1,
-                 world_boss: Union[Self, None] = None,
+    def __init__(self, world_boss: Union[Self, None] = None,
+                 boss_index: int = 1,
                  spawn_pattern_index: int = 13,
                  times_spawned: int = 0,
                  time_pattern_index: int = 3,
                  time_offset: int = 1,
                  last_spawn: Union[None, datetime] = None,
-                 tz_info: Any = pytz.timezone("US/Pacific")) -> None:
+                 display_tz: tzinfo = pytz.timezone("UTC")) -> None:
         """
         Args:
             boss_index (int, optional): Which boss entry we are at in the `_bosses` array. Defaults to 0.
@@ -23,9 +22,8 @@ class WorldBoss():
             times_spawned (int, optional): How many times the boss has spawned at current iteration. Defaults to 1.
             time_offset (int, optional): _description_. Which time offset to use from our `_time_offsets` array to 0.
             time_pattern_index (int, optional): Which position in the `_time_pattern`we are at. Defaults to 0.
-            last_spawn (Union[None, datetime], optional): Specific date of last known boss timer **MUST BE `pytz.timezone("US/Pacific")`**. Defaults to None.
-            tz_info (pytz) : Specify the timezone to be used for calculating the spawns. **ONLY SUPPORTS "US/Pacific" ATM**
-                _time_window's list would need to be adjusted for any offsets of timezone changes.
+            last_spawn (Union[None, datetime], optional): Specific date of last known boss timer **MUST BE `pytz.timezone("UTC")`**. Defaults to None.
+            display_tz (tzinfo) : Specify the timezone to be used for formatting the spawn times. 
         """
         self._file_dir = Path(__file__).parent
         self._json: Path = self._file_dir.joinpath("d4timer.json")
@@ -38,12 +36,14 @@ class WorldBoss():
         # **World Boss Timers**
         # https://www.reddit.com/r/diablo4/comments/14cv0t0/world_bosses_helltides_and_legion_events_are_all/
         # Last known spawn time
-        self._tz_info = tz_info
+        self._tz_info = pytz.timezone("UTC")
+        self._display_tz = display_tz
+        self._time_format = "%B, %d - %I:%M:%S %p"
+        _offset = 7
 
-        self._last_known_spawn: datetime = datetime(year=2023, month=10, day=2, hour=4, minute=41, tzinfo=self._tz_info)  # _time_pattern_index = 0, _time_offset_index= 1 , _boss_index= 0, _spawn_pattern_index = 0, _times_spawned = 1 of 3
+        self._last_known_spawn: datetime = datetime(year=2023, month=10, day=2, hour=self._time_diff(4, _offset), minute=41, tzinfo=self._tz_info)  # _time_pattern_index = 0, _time_offset_index= 1 , _boss_index= 0, _spawn_pattern_index = 0, _times_spawned = 1 of 3
         if last_spawn is not None:
             self._last_known_spawn = last_spawn
-
         self._last_boss: Union[None, dict[str, Any]] = None
         self._next_boss: Union[None, dict[str, Any]] = None
 
@@ -67,7 +67,8 @@ class WorldBoss():
         # 10/3/2023 5:19:11 AM - Wandering Death - 353.29 (Avarice?)   (2)
         # 10/3/2023 11:12:41 AM - Avarice -325.13 (Ashava?)                 (1)
         # 10/3/2023 4:37:54 PM - Ashava - 353.29                                  (2)
-        # 10/3/2023 10:31:24 PM - Ashava? -
+        # 10/3/2023 10:31:24 PM - Ashava? -                                         (3)
+        # 10/4/2023 4:25:00 AM - Wandering Death                                (1)
 
         # These specify how many times to repeat the time offset.
         self._time_pattern_index: int = time_pattern_index
@@ -80,46 +81,70 @@ class WorldBoss():
             timedelta(minutes=353, seconds=29),  # 29
             timedelta(minutes=325, seconds=13)]  # 13
 
-        # Time Windows (+2hrs if not inside these windows)(TZinfo = US/Pacific (UTC-8))
-        # TODO - Possible allow a user to pass in a UTC offset and dynamically adjust the time windows?
+        # Time Windows (+2hrs if not inside these windows)(TZinfo = UTC)
         self._time_window: list[tuple[timedelta, timedelta]] = [
-            (timedelta(hours=21, minutes=30), timedelta(hours=23, minutes=30)),  # 00:30 - 02:30
-            (timedelta(hours=3, minutes=30), timedelta(hours=5, minutes=30)),  # 06:30 - 08:30
-            (timedelta(hours=9, minutes=30), timedelta(hours=11, minutes=30)),  # 12:30 - 14:30
-            (timedelta(hours=15, minutes=30), timedelta(hours=17, minutes=30))]  # 18:30 - 20:30
+            (timedelta(hours=self._time_diff(21, _offset), minutes=30), timedelta(hours=self._time_diff(23, _offset), minutes=30)),  # 00:30 - 02:30
+            (timedelta(hours=self._time_diff(3, _offset), minutes=30), timedelta(hours=self._time_diff(5, _offset), minutes=30)),  # 06:30 - 08:30
+            (timedelta(hours=self._time_diff(9, _offset), minutes=30), timedelta(hours=self._time_diff(11, _offset), minutes=30)),  # 12:30 - 14:30
+            (timedelta(hours=self._time_diff(15, _offset), minutes=30), timedelta(hours=self._time_diff(17, _offset), minutes=30))]  # 18:30 - 20:30
 
     @property
     def last_boss(self) -> str | None:
         if self._last_boss is None:
             return
         if isinstance(self._last_boss['time'], datetime):
-            _time: str = self._last_boss['time'].strftime('%B, %d - %H:%M:%S')
-            return f"Last World Boss Spawn: {self._last_boss['name']} @ {_time} (+/- 1 minute)"
+            _time: str = self._last_boss['time'].astimezone(self._display_tz).strftime(self._time_format)
+            return f"Last World Boss Spawn: {self._last_boss['name']} @ {_time} {(self._display_tz)}"
 
     @property
     def next_boss(self) -> str | None:
         if self._next_boss is None:
             return
         if isinstance(self._next_boss['time'], datetime):
-            _time: str = self._next_boss['time'].strftime('%B, %d - %H:%M:%S')
-            return f"Next World Boss Spawn: {self._next_boss['name']} @ {_time} (+/- 1 minute)"
+            _time: str = self._next_boss['time'].astimezone(self._display_tz).strftime(self._time_format)
+            return f"Next World Boss Spawn: {self._next_boss['name']} @ {_time} {(self._display_tz)}"
 
     @property
     def copy_boss(self) -> Self:
         return copy.deepcopy(self)
 
+    def _time_diff(self, value: int, offset: Union[None, int], ):
+        # TODO - Docstring this
+        if offset == None:
+            return value
+        return (value + offset) % 24
+
+    def get_upcoming_boss(self) -> Union[None, dict[str, datetime]]:
+        """Updates the `_next_boss` attribute and returns the next boss to spawn!"""
+        # TODO - Docstring this
+        _world_boss: WorldBoss = self.copy_boss
+        _time: datetime = _world_boss._last_known_spawn
+        _found = False
+        while (_found) == False:
+            _time, boss = _world_boss.spawn_counter(last_time=_time)
+            if _time > datetime.now(tz=self._tz_info):
+                _found = True
+                self._next_boss = {"name": boss, "time": _time}
+                return self._next_boss
+
     def sequence_bosses(self, num: int) -> list:
+        # TODO - Docstring this
         _future_bosses: list[dict[str, Union[str, datetime]]] = []
-        _boss = WorldBoss(boss_index=self._boss_index, spawn_pattern_index=self._spawn_pattern_index, times_spawned=self._times_spawned, time_pattern_index=self._time_pattern_index, time_offset=self._time_offset_index, last_spawn=self._last_known_spawn)  # type:ignore
-        time: datetime = self._last_known_spawn
+        _next_boss: dict[str, datetime] | None = self.get_upcoming_boss()
+        if _next_boss is not None and isinstance(_next_boss["time"], datetime):
+            time = _next_boss["time"].astimezone(tz=self._display_tz)
+            _future_bosses.append({"name": _next_boss["name"], "time": f'{time.strftime(self._time_format)} {(self._display_tz)} (+/- 1 minute)'})
+        else:
+            time: datetime = self._last_known_spawn
+
         for x in range(0, num):
             if isinstance(time, datetime):
-                time, boss = _boss.spawn_counter(last_time=time)
-                _time = time.strftime('%B, %d - %H:%M:%S')
-            _future_bosses.append({"name": boss, "time": f'{_time} (+/- 1 minute)'})
+                time, boss = self.spawn_counter(last_time=time)
+                _time = time.astimezone(tz=self._display_tz).strftime(self._time_format)
+            _future_bosses.append({"name": boss, "time": f'{_time} {(self._display_tz)} (+/- 1 minute)'})
         return _future_bosses
 
-    def time_checker(self, after: timedelta, before: timedelta, time: Union[timedelta, datetime]) -> bool:
+    def time_checker(self, start_window: timedelta, end_window: timedelta, time: Union[timedelta, datetime]) -> bool:
         """
         Checks if our time's (hour, minutes) value lands in the window provided.
 
@@ -136,14 +161,16 @@ class WorldBoss():
             c_time = timedelta(hours=time.hour, minutes=time.minute)
         else:
             c_time: timedelta = time
-        if c_time >= after and c_time <= before:
+        if (start_window < end_window) and (c_time >= start_window and c_time <= end_window):
             # print(f"The time value is within the window {after} - {before}")
+            return True
+        elif (start_window > end_window) and (c_time >= start_window or c_time <= end_window):  # handle wrap on midnight
             return True
         else:
             # print(f"We are outside our time window {after} - {before}")
             return False
 
-    def next_spawn(self, boss_index: int, spawn_index: int, time_pattern: int, time_index: int, start_time: datetime) -> datetime:
+    def next_spawn_time(self, boss_index: int, spawn_index: int, time_pattern: int, time_index: int, start_time: datetime) -> datetime:
         """
         Calculates the next possible boss spawn time. Used in conjunction with `time_checker`.
 
@@ -151,7 +178,7 @@ class WorldBoss():
             boss_index (int): Current position in `_bosses` array.
             spawn_index (int): Current position in `_spawn_pattern` array.
             time_index (int): Current position in `_time_offsets` array
-            start_time (datetime): Usually a last known spawn datetime value; must be in `"US/Pacific"`
+            start_time (datetime): Usually a last known spawn datetime value; must be in `"EST/Eastern"`
 
         Returns:
             datetime: Possible spawn time as datetime object.
@@ -165,25 +192,26 @@ class WorldBoss():
         if time_pattern > (len(self._time_pattern) - 1):
             raise ValueError(f"It appears you have provided a time pattern out of range 0 - {len(self._time_pattern)-1}")
         offset: timedelta = self._time_offsets[time_index]
+        # TODO - We have an anomaly that causes our times to "slowly" creep further into the future.
         spawn_time: datetime = start_time + offset
         return spawn_time
 
     def spawn_counter(self, last_time: datetime) -> tuple[datetime, str]:
         """
-        Determines the next spawn time and if the time is in the correct window of spawn times.
+        Determines the next boss spawn and if the time is in the correct window of spawn times.
 
         Args:
-            last_time (datetime): Usually a last known spawn datetime value; must be in `"US/Pacific"`.
+            last_time (datetime): Usually a last known spawn datetime value; must be in `"EST/Eastern"`.
 
         Returns:
-            datetime: Next boss to be spawned and the exact datetime they will spawn.
+            tuple(time[datetime], boss[str]): Next boss to be spawned and the exact datetime they will spawn.
         """
         _spawn = False
-        self._last_boss = {"name": self._bosses[self._boss_index - 1], "time": last_time}
-        time: datetime = self.next_spawn(boss_index=self._boss_index, spawn_index=self._spawn_pattern_index, time_pattern=self._time_pattern_index, time_index=self._time_offset_index, start_time=last_time)
+        self._last_boss = {"name": self._bosses[self._boss_index], "time": last_time}
+        time: datetime = self.next_spawn_time(boss_index=self._boss_index, spawn_index=self._spawn_pattern_index, time_pattern=self._time_pattern_index, time_index=self._time_offset_index, start_time=last_time)
         val = timedelta(hours=time.hour, minutes=time.minute)
         for window in self._time_window:
-            if self.time_checker(after=window[0], before=window[1], time=val):
+            if self.time_checker(start_window=window[0], end_window=window[1], time=val):
                 _spawn = True
                 last_time = time
                 self._times_spawned += 1
@@ -213,10 +241,11 @@ class WorldBoss():
         # self._last_known_spawn = last_time
         # print(f"Next Boss Spawn: {cur_boss} @ {last_time.strftime('%B, %d - %I:%M:%S %p')} (+/- 1 minute)")
         # print(f"Next Boss Spawn: {cur_boss} @ {last_time.strftime('%B, %d - %H:%M:%S')} (+/- 1 minute)")
-        self._next_boss = {"name": cur_boss, "time": last_time}
+        # self._next_boss = {"name": cur_boss, "time": last_time}
         return last_time, cur_boss
 
     def json_load(self) -> datetime:
+        # TODO - Docstring this
         if self._json.is_file() is False:
             with open(self._json, "x") as jfile:
                 jfile.close()
@@ -229,22 +258,23 @@ class WorldBoss():
 
         for setting in ["last_spawn", "offset", "boss", "spawn", "lives"]:
             if setting not in data:
-                # print("Error loading setting", setting)
+                print("Error loading setting", setting)
                 return self._last_known_spawn
 
         self._last_known_spawn = datetime.fromtimestamp(int(data["last_spawn"]), tz=self._tz_info)
         self._time_offset_index = int(data["offset"])
         self._boss_index = int(data["boss"])
-        self._time_pattern_index = int(data["pattern"])
         self._spawn_pattern_index = int(data["spawn"])
         self._times_spawned = int(data["lives"])
         return self._last_known_spawn
 
     def json_save(self) -> None:
+        # TODO - Docstring this
+        # TODO - Using .timestamp() causes issues with future predictions when using the above .fromtimestamp
+        # Causes the time value to jump forward even though the timestamp() below is US/Pacific.
         data: dict[str, int | float] = {
             "last_spawn": self._last_known_spawn.timestamp(),
             "offset": self._time_offset_index,
-            "pattern": self._time_pattern_index,
             "boss": self._boss_index,
             "spawn": self._spawn_pattern_index,
             "lives": self._times_spawned
