f574db8276b1a0cd8deecf15d76499c2e2e0f591
k8thekat <cadwalladerkatelynn@gmail.com>
2023-12-08 20:34:51 -0800

    Base KumaCog class

---
+++ utils/cog.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+import os
+import logging
+
+
+class KumaCog(commands.Cog):
+    """Generic `commands.Cog` class inheritance to set some self attributes."""
+
+    def __init__(self, bot: commands.Bot) -> None:
+        super().__init__()
+        self._bot: commands.Bot = bot
+        self._name: str = os.path.basename(__file__).title()
+        self._logger = logging.getLogger()
+        self._logger.info(f'**SUCCESS** Initializing {self._name} ')
+        self.repo_url = "https://github.com/k8thekat/dpy_cogs"
